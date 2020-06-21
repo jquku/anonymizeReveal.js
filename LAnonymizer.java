5630d6c8a9fb94eb4ce7a76a5f9f05281be4dc8c
j_quad02 <j_quad02@uni-muenster.de>
2020-06-22 00:04:20 +0200

    added whole project

---
+++ LAnonymizer.java
@@ -0,0 +1,86 @@
+import java.io.IOException;
+
+import org.deidentifier.arx.AttributeType;
+import org.deidentifier.arx.Data;
+import java.nio.charset.Charset;
+import org.deidentifier.arx.criteria.KAnonymity;
+import org.deidentifier.arx.DataType;
+import org.deidentifier.arx.ARXResult;
+import org.deidentifier.arx.ARXConfiguration;
+import org.deidentifier.arx.ARXAnonymizer;
+import org.deidentifier.arx.DataHandle;
+import org.deidentifier.arx.aggregates.HierarchyBuilderRedactionBased;
+import org.deidentifier.arx.aggregates.HierarchyBuilderRedactionBased.Order;
+import org.deidentifier.arx.AttributeType.Hierarchy;
+
+class LAnonymizer {
+
+	public static void main(String[] args) throws IOException {
+
+			Data data1 = Data.create("students.csv", Charset.defaultCharset(), ',');
+			Data data2 = Data.create("tracked_sessions.csv",
+					Charset.defaultCharset(), ',');
+
+			data1.getDefinition().setDataType("user_token", DataType.INTEGER);
+			data1.getDefinition().setAttributeType("user_token",
+					AttributeType.INSENSITIVE_ATTRIBUTE);
+
+			data2.getDefinition().setDataType("student_id", DataType.INTEGER);
+			data2.getDefinition().setAttributeType("student_id",
+					AttributeType.INSENSITIVE_ATTRIBUTE);
+
+			data1.getDefinition().setDataType("id", DataType.INTEGER);
+			data1.getDefinition().setAttributeType("id",
+					AttributeType.INSENSITIVE_ATTRIBUTE);
+
+			data2.getDefinition().setDataType("id", DataType.INTEGER);
+			data2.getDefinition().setAttributeType("id",
+					AttributeType.INSENSITIVE_ATTRIBUTE);
+
+			data1.getDefinition().setDataType("created_at", DataType.createDate(
+					"yyyy-MM-dd HH:mm:ss.SSSSSS"));
+			data1.getDefinition().setDataType("updated_at", DataType.createDate(
+					"yyyy-MM-dd HH:mm:ss.SSSSSS"));
+
+			//data2.getDefinition().setDataType("tracking_json", DataType.STRING);
+			//data2	.getDefinition().setAttributeType("tracking_json",
+			//			AttributeType.INSENSITIVE_ATTRIBUTE);
+
+			//data2.getDefinition().setDataType("id", DataType.INTEGER);
+			data2.getDefinition().setDataType("created_at", DataType.createDate(
+					"yyyy-MM-dd HH:mm:ss.SSSSSS"));
+			data2.getDefinition().setDataType("updated_at", DataType.createDate(
+					"yyyy-MM-dd HH:mm:ss.SSSSSS"));
+
+
+			HierarchyBuilderRedactionBased<?> builder =
+					HierarchyBuilderRedactionBased.create(Order.RIGHT_TO_LEFT,
+					Order.RIGHT_TO_LEFT, ' ', '*');
+
+			data1.getDefinition().setAttributeType("created_at", builder);
+			data1.getDefinition().setAttributeType("updated_at", builder);
+
+			data2.getDefinition().setAttributeType("created_at", builder);
+			data2.getDefinition().setAttributeType("updated_at", builder);
+
+			//instance of the anonymizer
+			ARXAnonymizer anonymizer = new ARXAnonymizer();
+
+			// ARX configuration
+			ARXConfiguration config = ARXConfiguration.create();
+			config.addPrivacyModel(new KAnonymity(2));
+			config.setSuppressionLimit(1d);
+			config.setHeuristicSearchStepLimit(1000);
+			config.setHeuristicSearchEnabled(true);
+
+			ARXResult result1 = anonymizer.anonymize(data1, config);
+			ARXResult result2 = anonymizer.anonymize(data2, config);
+
+			//obtain various data representations
+			DataHandle optimal1 = result1.getOutput();
+			DataHandle optimal2 = result2.getOutput();
+
+			optimal1.save("students_new.csv", ',');
+			optimal2.save("tracked_sessions_new.csv", ',');
+	}
+}
---
+++ anonymize.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+psql -h localhost -p 5432 -U postgres << EOF
+\c learning_analytics_development
+create table if not exists students_anonymized as (select * from students) with no data;
+create table if not exists tracked_sessions_anonymized as (select * from tracked_sessions) with no data;
+ALTER TABLE students_anonymized ALTER COLUMN created_at TYPE text, ALTER COLUMN updated_at TYPE text;
+ALTER TABLE tracked_sessions_anonymized ALTER COLUMN created_at TYPE text, ALTER COLUMN updated_at TYPE text;
+\COPY students (id, user_token, created_at, updated_at) TO 'students.csv' CSV HEADER
+\COPY tracked_sessions(id, student_id, created_at, updated_at) TO 'tracked_sessions.csv' CSV HEADER
+\COPY tracked_sessions(tracking_json) TO 'tracking_json.csv' CSV HEADER
+EOF
+java LAnonymizer -cp ./libarx-3.8.0.jar
+sed -i '1d' students_new.csv
+sed -i '1d' tracked_sessions_new.csv
+sed -i '1d' tracking_json.csv
+psql -h localhost -p 5432 -U postgres << EOF
+\c learning_analytics_development
+\COPY students_anonymized(id, user_token, created_at, updated_at) FROM 'students_new.csv' WITH (FORMAT csv);
+\COPY tracked_sessions_anonymized(id, student_id, created_at, updated_at) FROM 'tracked_sessions_new.csv' WITH (FORMAT csv);
+update tracked_sessions_anonymized set tracking_json = tracked_sessions.tracking_json from tracked_sessions_anonymized t1 inner join tracked_sessions on t1.id = tracked_sessions.id;
+EOF
+
+
